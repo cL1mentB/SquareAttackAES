84355d51555887c44254f115117dcf06317a647f
Loïse <maelle.sinilo@etu.unilim.fr>
2021-12-09 16:36:02 +0100

    tests

--- AES.c
+++ AES.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-    printf("Test\n");
+
+	unsigned char * a = "0x6bc1bee22E409F96e93d7e117393172a";
+    for(int i=0; i<16; i++){
+    	printf("%02.2x\n", a[i]);
+    }
 }

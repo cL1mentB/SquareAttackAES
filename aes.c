714ce3d8f55e7fc8c3c2d50a9eb2cc64ed78bcd0
linoushbo <lina.bouchfanj@etu.unilim.fr>
2021-12-09 16:33:47 +0100

    modif

--- aes.c
+++ aes.c
@@ -5,7 +5,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
 
 //Fonction qui recupère un message de 16 octets et le transforme en matrice 4X4
-int initialisation(unsigned char state[4][4], unsigned char* message)
+void initialisation(unsigned char state[4][4], unsigned char* message)
 {
 
 	int a=0; 
@@ -16,10 +16,13 @@ int initialisation(unsigned char state[4][4], unsigned char* message)
 			state[i][j] = message[a];
 			a = a + 1 ;
 		}
-
 	}
-	return 1; 
 }
+
+
+//Fonction AddRoundKey prend en entrée une matrice
+// et un sous-clef K_i de 128 bits et renvoie le XOR des deux
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// Main //////////////////////////////////////////////////

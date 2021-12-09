1da011957e4ed4c854bb9fd2685e99e5203dd455
linoushbo <lina.bouchfanj@etu.unilim.fr>
2021-12-09 16:26:47 +0100

    fonction init

--- aes.c
+++ aes.c
@@ -4,19 +4,21 @@
 /////////////////////////////////// Fonctions /////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-
-int initialisation(char message)
+//Fonction qui recupère un message de 16 octets et le transforme en matrice 4X4
+int initialisation(unsigned char state[4][4], unsigned char* message)
 {
-	//teste longeur du message 
-	if (len(message) != 16 )
-	{
-		return 0; 
-	}
-	else 
-	{
-		return 1; 
-	}
 
+	int a=0; 
+	for(int i=0 ; i<4; i++)
+	{
+		for(int j=0; j<4; j++)
+		{
+			state[i][j] = message[a];
+			a = a + 1 ;
+		}
+
+	}
+	return 1; 
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////
@@ -26,10 +28,15 @@ int initialisation(char message)
 int main(int argc, char* argv[])
 {
 
-char message1; 
-message1 ="6bc1bee22E409F96e93d7e117393172a"; 
+	//déclaration du message 
+	unsigned char message1[16] = {0x6b , 0xc1, 0xbe, 0xe2, 0x2E, 0x40 , 0x9F, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a}; 
+ 
+	//declaration de la matrice AES
+	unsigned char state[4][4];
+	//remplissage de la matrice 
+	initialisation(state, message1);
+	//verif
+	printf("%x\n",state[1][1]);
 
-int a = initialisation(message1);
-printf("%d\n", a );
 
 }

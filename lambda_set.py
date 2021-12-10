8f3e54447e4ad67b397746f01fa1cb66b8a15e80
Loïse <maelle.sinilo@etu.unilim.fr>
2021-12-10 17:26:04 +0100

    set

--- lambda_set.py
+++ lambda_set.py
@@ -2,9 +2,18 @@
 
 import sys
 
-l = [[hex(32) for i in range (16)] for i in range(256)]
+l = [['{:x}'.format(107) for i in range (16)] for i in range(256)]
 
 for i in range (0,256):
-	l[i][5] = hex(i)
+	l[i][5] = '{:x}'.format(i)
 
-print(l)
+
+
+
+for i in range (0,256):
+	ligne = ""
+	for j in range (0,16):
+		ligne += l[i][j] 
+	sys.stdout=open("lambda_set.txt","a")
+	print(ligne)
+	sys.stdout.close()
